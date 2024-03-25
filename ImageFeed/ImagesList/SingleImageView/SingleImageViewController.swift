bd1e47b3c4c1d213c9cc4efb155c9b5cd8a2794c
Fen1x <baurasm@bk.ru>
2024-03-25 22:57:12 +0300

    потерял экран так как не закомил его и всзал его же у автора

---
+++ ImageFeed/ImagesList/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet private var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+}
