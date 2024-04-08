6c50df7f2bbea4fee8e4da41da1ee655a6603a8d
Fen1x <baurasm@bk.ru>
2024-04-08 16:05:21 +0300

    Добавил код Auth и добавил экран для него

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "zlMhopfcl25fMY7_ur-5gBtR8C-JeMFnOCfrXS_FUmk"
+    static let secretKey = "iW-WFbqJTqLXWQVk5PSynfJOAXtCqFU3A4GadNO1_Mo"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
