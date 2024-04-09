8b3fa73709ce6c5110ac2c1ab058f7f65504c2bc
Fen1x <baurasm@bk.ru>
2024-04-09 14:09:25 +0300

    добавление кода и новых экранов

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -1,9 +1,7 @@
 import Foundation
 
-enum Constants {
-    static let accessKey = "zlMhopfcl25fMY7_ur-5gBtR8C-JeMFnOCfrXS_FUmk"
-    static let secretKey = "iW-WFbqJTqLXWQVk5PSynfJOAXtCqFU3A4GadNO1_Mo"
-    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
-}
+let AccessKey = "zlMhopfcl25fMY7_ur-5gBtR8C-JeMFnOCfrXS_FUmk"
+let SecretKey = "iW-WFbqJTqLXWQVk5PSynfJOAXtCqFU3A4GadNO1_Mo"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
