fe903092794f7d337a6eb86c5527b92d5e63b085
Fen1x <baurasm@bk.ru>
2024-08-16 15:48:19 +0300

    ImagesListService.fetchPhotosNextPage содержит implicit unwrap. В методе changeLike сильно захвачен self. Исправил

--- ImageFeed/ImagesList/ImagesListService.swift
+++ ImageFeed/ImagesList/ImagesListService.swift
@@ -10,7 +10,7 @@ import Foundation
 final class ImagesListService {
     static let shared = ImagesListService()
     static let didChangeNotification = Notification.Name(rawValue: "ImagesListServiceDidChange")
-    private (set) var photos: [Photo] = []
+    private(set) var photos: [Photo] = []
     private var currentTask: URLSessionTask?
     private var lastLoadedPage: Int?
     private let urlSession = URLSession.shared
@@ -23,7 +23,9 @@ final class ImagesListService {
         assert(Thread.isMainThread)
         
         guard currentTask == nil else { return }
-        let nextPage = lastLoadedPage == nil ? 1 : lastLoadedPage! + 1
+        
+        // Использование дефолтного значения через ??
+        let nextPage = (lastLoadedPage ?? 0) + 1
         page = nextPage
         
         guard let authToken = OAuth2TokenStorage.shared.token else {
@@ -35,16 +37,12 @@ final class ImagesListService {
             return
         }
         
-        let task = urlSession.objectTask(for: request) { [ weak self] (result: Result<[PhotoResult], Error>) in
-            guard let self = self else {return}
+        let task = urlSession.objectTask(for: request) { [weak self] (result: Result<[PhotoResult], Error>) in
+            guard let self = self else { return }
             DispatchQueue.main.async {
                 switch result {
                 case .success(let photoResults):
-                    if self.lastLoadedPage == nil {
-                        self.lastLoadedPage = 1
-                    } else {
-                        self.lastLoadedPage! += 1
-                    }
+                    self.lastLoadedPage = (self.lastLoadedPage ?? 0) + 1
                     let newPhotos = photoResults.map { Photo($0, date: self.dateFormatter) }
                     self.photos.append(contentsOf: newPhotos)
                     
@@ -60,7 +58,7 @@ final class ImagesListService {
         task.resume()
     }
     
-    func changeLike(photoId: String, isLike:Bool, _ completion: @escaping(Result<Void, Error>)->Void) {
+    func changeLike(photoId: String, isLike: Bool, _ completion: @escaping (Result<Void, Error>) -> Void) {
         if currentTask != nil {
             currentTask?.cancel()
         }
@@ -68,73 +66,84 @@ final class ImagesListService {
         guard let request = likeRequest(photoId: photoId, isLike: isLike) else {
             return
         }
-        let task = urlSession.objectTask(for: request) { (result: Result<PhotoLiked, Error>) in
+        
+        // Добавление слабой ссылки на self
+        let task = urlSession.objectTask(for: request) { [weak self] (result: Result<PhotoLiked, Error>) in
+            guard let self = self else { return }
             DispatchQueue.main.async {
                 switch result {
                 case .success:
-                    if let index = self.photos.firstIndex(where: {$0.id == photoId}) {
+                    if let index = self.photos.firstIndex(where: { $0.id == photoId }) {
                         let photo = self.photos[index]
                         
-                        let newPhotoResult = PhotoResult(id: photo.id,
-                                                         width: Int(photo.size.width),
-                                                         height: Int(photo.size.height),
-                                                         createdAt: photo.createdAt?.description,
-                                                         description: photo.welcomeDescription,
-                                                         urls: UrlsResult(full: photo.largeImageURL, regular: photo.regularImageURL,
-                                                                          small: photo.smallImageURL, thumb: photo.thumbImageURL
-                                                                         ),
-                                                         likedByUser: !photo.isLiked)
+                        let newPhotoResult = PhotoResult(
+                            id: photo.id,
+                            width: Int(photo.size.width),
+                            height: Int(photo.size.height),
+                            createdAt: photo.createdAt?.description,
+                            description: photo.welcomeDescription,
+                            urls: UrlsResult(
+                                full: photo.largeImageURL,
+                                regular: photo.regularImageURL,
+                                small: photo.smallImageURL,
+                                thumb: photo.thumbImageURL
+                            ),
+                            likedByUser: !photo.isLiked
+                        )
                         
-                        let newPhoto = Photo(newPhotoResult, date: self.dateFormatter)
-                        
-                        self.photos[index] = newPhoto
-                        completion(.success(()))
+                        // Обновление информации о фото
+                        self.photos[index] = Photo(newPhotoResult, date: self.dateFormatter)
+                        NotificationCenter.default.post(name: ImagesListService.didChangeNotification, object: nil)
                     }
+                    
+                    completion(.success(()))
+                    
                 case .failure(let error):
-                    assertionFailure("like error: \(error)")
+                    completion(.failure(error))
                 }
             }
         }
         self.currentTask = task
         task.resume()
     }
-    
-    private func makeRequest(authToken: String, page: Int) -> URLRequest? {
-        guard var urlComponents = URLComponents(string: "https://api.unsplash.com/photos") else {
-            assertionFailure("Error with URL")
-            return nil
-        }
-        urlComponents.queryItems = [
-            URLQueryItem(name: "page", value: "\(page)"),
-            URLQueryItem(name: "per_page", value: "10")
-        ]
-        guard let url = urlComponents.url else {
-            assertionFailure("Error with URL")
-            return nil
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
-        return request
-    }
-    
-    private func likeRequest(photoId: String, isLike: Bool) -> URLRequest? {
-        let baseURL = "https://api.unsplash.com"
-        let likeURL = "\(baseURL)/photos/\(photoId)/like"
-        
-        guard let url = URL(string: likeURL) else {
-            assertionFailure("Error with URL")
-            return nil
-        }
-        var request = URLRequest(url: url)
-        request.httpMethod = isLike ? "POST" : "DELETE"
-        
-        guard let authToken = OAuth2TokenStorage.shared.token else {
-            return nil
-        }
-        request.setValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
-        
-        return request
-    }
 }
+
+private func makeRequest(authToken: String, page: Int) -> URLRequest? {
+    guard var urlComponents = URLComponents(string: "https://api.unsplash.com/photos") else {
+        assertionFailure("Error with URL")
+        return nil
+    }
+    urlComponents.queryItems = [
+        URLQueryItem(name: "page", value: "\(page)"),
+        URLQueryItem(name: "per_page", value: "10")
+    ]
+    guard let url = urlComponents.url else {
+        assertionFailure("Error with URL")
+        return nil
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    request.setValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
+    return request
+}
+
+private func likeRequest(photoId: String, isLike: Bool) -> URLRequest? {
+    let baseURL = "https://api.unsplash.com"
+    let likeURL = "\(baseURL)/photos/\(photoId)/like"
+    
+    guard let url = URL(string: likeURL) else {
+        assertionFailure("Error with URL")
+        return nil
+    }
+    var request = URLRequest(url: url)
+    request.httpMethod = isLike ? "POST" : "DELETE"
+    
+    guard let authToken = OAuth2TokenStorage.shared.token else {
+        return nil
+    }
+    request.setValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
+    
+    return request
+}
+
