b41fbff4e0cd5ebf271f797866b2c6b99d6da5e2
Fen1x <baurasm@bk.ru>
2024-09-24 15:10:45 +0300

    исправил одну ошибку

--- ImageFeed/CacheManager/CacheManager.swift
+++ ImageFeed/CacheManager/CacheManager.swift
@@ -1,14 +1,1 @@
-import Foundation
-import Kingfisher
 
-final class CacheManager {
-    
-    static func clean() {
-        let cache = ImageCache.default
-        cache.clearMemoryCache()
-        cache.clearDiskCache()
-        cache.backgroundCleanExpiredDiskCache()
-        cache.cleanExpiredMemoryCache()
-        cache.clearCache()
-    }
-}
